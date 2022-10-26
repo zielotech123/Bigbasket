95635aef95ef6f7b09272242b30abe729dc4b0e2
indrani sethi <indranisethi123@gmail.com>
2022-10-26 16:20:50 +0000

    Added .gitignore file

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+#This script is going to integrate the CICD.
--- integrate.java
+++ integrate.java
@@ -2,4 +2,4 @@
 
 # This code is prepared for integrating the other source codes
 
-cp Product.java Product_`date +'%d-%m-%Y'`.java
+hello,cp Product.java Product_`date +'%d-%m-%Y'`.java
